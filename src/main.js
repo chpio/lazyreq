d2a9a7b5a93813e8a126c829c776fe3533ee0127
Thomas Heck <t@b128.net>
2015-08-12 23:18:34 +0200

    + lazyreq script

---
+++ src/main.js
@@ -0,0 +1,29 @@
+export default function lazyReq(req, modules) {
+	const ret = {};
+
+	Object.keys(modules).forEach(mKey => {
+		const mVal = modules[mKey];
+		Object.defineProperty(ret, mKey, {
+			configurable: true,
+			get() {
+				let mod;
+				if(typeof mVal === 'function') {
+					mod = mVal(mKey);
+				} else if(typeof mVal === 'string') {
+					mod = req(mVal);
+				} else {
+					throw new Error(`Invalid module type of ${mKey}`);
+				}
+
+				Object.defineProperty(ret, mKey, {
+					writable: false,
+					value: mod,
+				});
+
+				return mod;
+			}
+		});
+	});
+
+	return ret;
+}
