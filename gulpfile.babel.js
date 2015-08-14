bce6f480ec278b44ccb9a6d9f980415e6d818398
Thomas Heck <t@b128.net>
2015-08-14 12:19:55 +0200

    fix code style in gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,25 @@
-'use strict';
+const gulp = require('gulp');
 
-var gulp = require('gulp');
-
-var $ = require('lazyreq')(require, {
+const $ = require('lazyreq')(require, {
 	newer: 'gulp-newer',
 	cached: 'gulp-cached',
 	babel: 'gulp-babel',
 	eslint: 'gulp-eslint',
 });
 
-gulp.task('build', function() {
-	return gulp.src('./src/**/*.js')
+gulp.task('build', () =>
+	gulp.src('./src/**/*.js')
 		.pipe($.newer('./build'))
 		.pipe($.babel({
 			sourceMaps: 'inline',
 		}))
-		.pipe(gulp.dest('./build'));
-});
+		.pipe(gulp.dest('./build'))
+);
 
-gulp.task('lint', function() {
-	return gulp.src(['./src/**/*.js', './gulpfile.babel.js'])
+gulp.task('lint', () =>
+	gulp.src(['./src/**/*.js', './gulpfile.babel.js'])
 		.pipe($.cached('lint'))
 		.pipe($.eslint())
 		.pipe($.eslint.format())
-		.pipe($.eslint.failAfterError());
-});
+		.pipe($.eslint.failAfterError())
+);
