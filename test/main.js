0cb73dfd5e8f2d66956089a5234d0075d1d5e813
Thomas Heck <t@b128.net>
2015-08-28 22:36:54 +0200

    + array require tests

--- test/main.js
+++ test/main.js
@@ -30,17 +30,38 @@ it('function require', () => {
 	assert.equal(ret.key2, 'func require val2');
 });
 
+it('array require', () => {
+	const ret = lazyReq(
+		(mod) => {
+			return {
+				test: `test val ${mod}`,
+			};
+		},
+		{
+			arrStr: ['arr-str-mod', 'test'],
+			arrFunc: ['arr-func-mod', (v) => `func ${v.test}`],
+		}
+	);
+
+	assert.equal(ret.arrStr, 'test val arr-str-mod');
+	assert.equal(ret.arrFunc, 'func test val arr-func-mod');
+});
+
 it('invalid require', () => {
 	const ret = lazyReq(
 		(mod) => mod,
 		{
 			num: 123,
-			arr: [],
+			arrEmpty: [],
+			arrStr: [{str: 'str'}, '---nonExistent'],
+			arrObj: ['mod-name', {}],
 			obj: {},
 		}
 	);
 
 	assert.throws(() => ret.num);
-	assert.throws(() => ret.arr);
+	assert.throws(() => ret.arrEmpty);
+	assert.throws(() => ret.arrStr);
+	assert.throws(() => ret.arrObj);
 	assert.throws(() => ret.obj);
 });
